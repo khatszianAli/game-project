36f11ca6cb192cff9e8385d6e7279e6056bea7c3
ka12438 <ka12438@auca.kg>
2025-06-11 15:34:18 +0600

    add some stuff

---
+++ sokoban/build/CMakeFiles/sokoban.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/newowner/CLionProjects/simple-sokoban-project/sokoban/globals.cpp" "CMakeFiles/sokoban.dir/globals.cpp.o" "gcc" "CMakeFiles/sokoban.dir/globals.cpp.o.d"
+  "/Users/newowner/CLionProjects/simple-sokoban-project/sokoban/images.cpp" "CMakeFiles/sokoban.dir/images.cpp.o" "gcc" "CMakeFiles/sokoban.dir/images.cpp.o.d"
+  "/Users/newowner/CLionProjects/simple-sokoban-project/sokoban/levels.cpp" "CMakeFiles/sokoban.dir/levels.cpp.o" "gcc" "CMakeFiles/sokoban.dir/levels.cpp.o.d"
+  "/Users/newowner/CLionProjects/simple-sokoban-project/sokoban/levels_data.cpp" "CMakeFiles/sokoban.dir/levels_data.cpp.o" "gcc" "CMakeFiles/sokoban.dir/levels_data.cpp.o.d"
+  "/Users/newowner/CLionProjects/simple-sokoban-project/sokoban/levels_globalc.cpp" "CMakeFiles/sokoban.dir/levels_globalc.cpp.o" "gcc" "CMakeFiles/sokoban.dir/levels_globalc.cpp.o.d"
+  "/Users/newowner/CLionProjects/simple-sokoban-project/sokoban/player_globals.cpp" "CMakeFiles/sokoban.dir/player_globals.cpp.o" "gcc" "CMakeFiles/sokoban.dir/player_globals.cpp.o.d"
+  "/Users/newowner/CLionProjects/simple-sokoban-project/sokoban/sokoban.cpp" "CMakeFiles/sokoban.dir/sokoban.cpp.o" "gcc" "CMakeFiles/sokoban.dir/sokoban.cpp.o.d"
+  "/Users/newowner/CLionProjects/simple-sokoban-project/sokoban/sounds.cpp" "CMakeFiles/sokoban.dir/sounds.cpp.o" "gcc" "CMakeFiles/sokoban.dir/sounds.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sokoban/build/CMakeFiles/sokoban.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sokoban.dir/globals.cpp.o"
+  "CMakeFiles/sokoban.dir/globals.cpp.o.d"
+  "CMakeFiles/sokoban.dir/images.cpp.o"
+  "CMakeFiles/sokoban.dir/images.cpp.o.d"
+  "CMakeFiles/sokoban.dir/levels.cpp.o"
+  "CMakeFiles/sokoban.dir/levels.cpp.o.d"
+  "CMakeFiles/sokoban.dir/levels_data.cpp.o"
+  "CMakeFiles/sokoban.dir/levels_data.cpp.o.d"
+  "CMakeFiles/sokoban.dir/levels_globalc.cpp.o"
+  "CMakeFiles/sokoban.dir/levels_globalc.cpp.o.d"
+  "CMakeFiles/sokoban.dir/player_globals.cpp.o"
+  "CMakeFiles/sokoban.dir/player_globals.cpp.o.d"
+  "CMakeFiles/sokoban.dir/sokoban.cpp.o"
+  "CMakeFiles/sokoban.dir/sokoban.cpp.o.d"
+  "CMakeFiles/sokoban.dir/sounds.cpp.o"
+  "CMakeFiles/sokoban.dir/sounds.cpp.o.d"
+  "sokoban"
+  "sokoban.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sokoban.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
